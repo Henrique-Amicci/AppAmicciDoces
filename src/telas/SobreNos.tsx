8e60f4c4dc2659f00aae95693f303ee599a4d3ea
Henrique-Amicci <henriquesmamicci@gmail.com>
2025-02-27 20:02:28 -0300

    Aplicativo AmicciDoces

---
+++ src/telas/SobreNos.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {Text, StatusBar} from "react-native";
+
+import Texto from "../componentes/Texto";
+
+export default function SobreNos(){
+    return <>
+    <StatusBar/>
+    <Texto>Hello World!</Texto>
+    </>
+}
