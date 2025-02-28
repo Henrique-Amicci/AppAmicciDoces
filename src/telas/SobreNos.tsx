8f33467ae6a6b511dbf04d40443a31b80ede221b
Henrique-Amicci <henriquesmamicci@gmail.com>
2025-02-27 22:10:50 -0300

    Add files via upload

--- src/telas/SobreNos.tsx
+++ src/telas/SobreNos.tsx
@@ -1,11 +1,23 @@
 import React from "react";
-import {Text, StatusBar} from "react-native";
+import {Text, StatusBar, StyleSheet, View, Image} from "react-native";
 
 import Texto from "../componentes/Texto";
 
 export default function SobreNos(){
-    return <>
+    return <View style={estilo.fundo}>
     <StatusBar/>
+    <Image source={require('../../assets/LEGO_logo.svg.png')}style={estilo.logo} resizeMode="contain"></Image>
     <Texto>Hello World!</Texto>
-    </>
+    </View>
 }
+
+const estilo = StyleSheet.create({
+    fundo: {
+        backgroundColor: "purple",
+    },
+    logo: {
+        width: 300,
+        height: 300,
+        alignSelf: "center"
+    }
+})
