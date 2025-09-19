b9b73fd0cf8897dcb421c9f421d25e31397c744b
Henrique-Amicci <henriquesmamicci@gmail.com>
2025-09-18 21:48:57 -0300

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -1,58 +1,109 @@
 import React from "react";
-import { useFonts, Montserrat_700Bold, Montserrat_400Regular } from "@expo-google-fonts/montserrat";
-import { View } from "react-native";
+import { useFonts, 
+         Montserrat_400Regular, 
+         Montserrat_700Bold } 
+         from "@expo-google-fonts/montserrat";
+import { TouchableOpacity, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"; 
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import SobreNos from "./src/telas/SobreNos"; 
-import Produto from "./src/telas/Produtos";
-import MockProdutos from "./src/mocks/ListaProduto";
-function  MenuProdutos(){
-  return <Produto {...MockProdutos}/>
+import { useAudioPlayer } from "expo-audio";
+
+import SobreNos from "./src/telas/SobreNos";
+
+//Menu PRODUTOS
+import Produto from './src/telas/Produtos';
+import MockProdutos from './src/mocks/listaProduto';
+
+
+//Menu Perfil
+import Perfil from './src/telas/Perfil';
+
+//Menu Desejos
+import ListaDesejo from './src/telas/ListaDesejos';
+import Texto from "./src/componentes/Texto";
+
+
+//Função para execução do áudio
+function MenuAudio(){
+  const audioSource = require('./assets/RIP_MAO_ZEDONG_HOSTED_BY馬爾瓦多666DJ.mp3');
+  const player = useAudioPlayer(audioSource);
+
+  //Configuração do controle On/Off
+  const onOff = () => {
+    if(player.playing){
+      player.pause();
+    }else {
+      if(player.currentTime === player.duration){
+        player.seekTo(0);
+      }else{
+        player.play();
+      }
+    }
+  }
+  
+
+  return <TouchableOpacity onPress={onOff}>
+          <Texto>🎧On/Off</Texto>
+          </TouchableOpacity>
 }
-// Configuração do Menu
+
+function MenuProdutos(){
+  return <Produto {...MockProdutos} />
+}
+
+//Configuração do Menu
 const Tab = createBottomTabNavigator();
-function Menu(){
-  return <Tab.Navigator screenOptions={({route}) => ({
-              tabBarIcon: ({focused, color, size}) => {
-                let iconName: string;
-                if(route.name === "Sobre Nós"){
-                  iconName = focused
-                  ? 'home'
-                  : 'home-outline'
-                }else if(route.name === "Produtos"){
-                  iconName = focused
-                  ? 'car-sport'
-                  : 'car-sport-outline'
-                }else if(route.name === "Lista de Desejos"){
-                  iconName = focused
-                  ? 'list'
-                  : 'list-outline'
-                }else if(route.name === "Perfil"){
-                  iconName = focused
-                  ? 'person-circle'
-                  : 'person-circle-outline'
-                }
-                return <Ionicons name={iconName} size={size} color={color}/>
-              },
-              tabBarActiveTintColor: 'purple',
-              tabBarInactiveTintColor: 'gray',
-              headerShown: false,                
-            })}>
-            <Tab.Screen name="Sobre Nós" component={SobreNos}/>
-            <Tab.Screen name="Produtos" component={MenuProdutos}/>
-            <Tab.Screen name="Lista de Desejos" component={SobreNos}/>
-            <Tab.Screen name="Perfil" component={SobreNos}/>
+
+function Menu() {
+  return <Tab.Navigator
+              screenOptions={({route})=>({
+                  tabBarIcon: ({focused, color, size})=>{
+                    let iconName: any;
+
+                    if(route.name ==="Sobre Nós"){
+                      iconName = focused
+                      ? 'storefront'
+                      : 'storefront-outline';
+                    } else if(route.name ==="Produtos"){
+                      iconName = focused
+                      ? 'list'
+                      : 'list-outline';
+                    }else if(route.name ==="Lista de Desejos"){
+                      iconName = focused
+                      ? 'heart'
+                      : 'heart-outline';
+                    } else if(route.name ==="Perfil"){
+                      iconName = focused
+                      ? 'person'
+                      : 'person-outline';
+                    }
+                    return <Ionicons name={iconName} size={size} color={color}/>
+                  },
+                  tabBarActiveTintColor:'purple',
+                  tabBarInactiveTintColor: 'gray',
+                  headerShown: false,
+              })}>
+            <Tab.Screen name="Sobre Nós" component={SobreNos} />
+            <Tab.Screen name="Produtos" component={MenuProdutos} />
+            <Tab.Screen name="Lista de Desejos" component={ListaDesejo} />
+            <Tab.Screen name="Perfil" component={Perfil} />
         </Tab.Navigator>
 }
+
 export default function App() {
-  // Configuração da fonte para o app
-  const [fonteCarregada] = useFonts({"FonteRegular": Montserrat_400Regular, "FonteNegrito": Montserrat_700Bold});
-  // Verifica se a fonte foi carregada, se não, não exibe nada
+
+  //Configuração da fonte para o app
+  const [fonteCarregada] = useFonts({"FonteRegular": Montserrat_400Regular,
+                                     "FonteBold": Montserrat_700Bold});
+
+  //Verifica se a fonte foi carregada, se não, não exibe nada
   if(!fonteCarregada){
     return <View/>
   }
+
   return <NavigationContainer>
-    <Menu/>
-  </NavigationContainer>
+              <Menu />
+              <MenuAudio/>
+        </NavigationContainer>
 }
